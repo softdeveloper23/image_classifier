1b18cc99ae04ff024ef5b92d5ff81e6d460c4245
softdeveloper23 <brannongarrett77@gmail.com>
2025-05-16 16:59:44 +0200

    Added classify_image function

--- main.py
+++ main.py
@@ -20,7 +20,16 @@ def preprocess_image(image):
 
     return img
 
-
+def classify_image(model, image):
+    try:
+        processed_image = preprocess_image(image)
+        predictions = model.predict(processed_image)
+        decoded_predictions = decode_predictions(predictions, top=3)[0]
+        
+        return decoded_predictions
+    except Exception as e:
+        st.error(f"Error classifying image: {e}")
+        return None
 
     
     
