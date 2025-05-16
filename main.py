da6afe8719e842eb3b4f9b4f38218a7d6f210cae
softdeveloper23 <brannongarrett77@gmail.com>
2025-05-16 16:54:50 +0200

    Added new functions

--- main.py
+++ main.py
@@ -8,8 +8,22 @@ from tensorflow.keras.applications.mobilenet_v2 import (
 )
 from PIL import Image
 
+def load_model():
+    model = MobileNetV2(weights="imagenet")
+    return model
+
+def preprocess_image(image):
+    img = np.array(image)
+    img = cv2.resize(img, (224, 224))
+    img = preprocess_input(img)
+    img = np.expand_dims(img, axis=0)
+
+    return img
 
 
+
+    
+    
 
 
 
