457f39aa01975e8f1b90f4894126f41f553b97bc
softdeveloper23 <brannongarrett77@gmail.com>
2025-05-16 16:48:29 +0200

    Added imports

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from image-classifier!")
+import cv2
+import numpy as np
+import streamlit as st
+from tensorflow.keras.applications.mobilenet_v2 import (
+    MobileNetV2,
+    preprocess_input,
+    decode_predictions,
+)
+from PIL import Image
 
 
-if __name__ == "__main__":
-    main()
+
+
+
+
+
+
